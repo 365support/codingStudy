a5ae0535b280d8c008c99239b3d8166ee62983a0
HeeJeYang <ghjadmb21@gmail.com>
2022-04-20 01:24:19 +0900

    양희제: 프로그래머스 60057번 문제 완료

--- yangheeje/60057.js
+++ yangheeje/60057.js
@@ -1,4 +1,34 @@
-function solution(s) {
-  var answer = 0;
-  return answer;
+function solution(S) {
+  if (S.length === 1) return 1;
+const halfOfS = Math.floor(S.length / 2);
+
+let answer = 1001;
+for (let i = 1; i <= halfOfS; i++) {
+  let index = 0;
+  let result = 0;
+  let duplicateCount = 1;
+      const cutArr = [];
+  do {
+    cutArr.push(S.substr(index, i));
+    index += i;
+  } while (index + i <= S.length)
+  result += S.length - index
+  cutArr.reduce((acc, cur, idx, src) => {
+    if (acc === cur) {
+      duplicateCount++  
+    } else {
+      if (duplicateCount >= 2) {
+          result += (duplicateCount + "").length;
+          duplicateCount = 1
+      }
+      result += i
+    }
+      if (idx === src.length - 1) {
+        duplicateCount >= 2 ? result += i + (duplicateCount + "").length : result += i;
+      }
+    return cur;
+  })
+  answer = Math.min(answer, result)
+}
+return answer;
 }
